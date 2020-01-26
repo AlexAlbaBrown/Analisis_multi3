a688cf4372c81a04cc3ac498716f7e89442a71f4
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2020-01-26 17:12:24 -0300

    prácticos del curso

---
+++ Prácticos/Práctico 3.R
@@ -0,0 +1,20 @@
+####################
+#### PRÁCTICO 3 ####
+####################
+
+rm(list=ls())
+setwd("G:/UdelaR/CCEEA/Semestre 7/Análisis Multivariado I/Prácticos")
+
+#### EJERCICIO 1 ####
+
+for(i in -10:10){
+      d=i
+      S=matrix(data=c(1+d,1,1,1,1+d,1,1,1,1+d), nrow=3, ncol=3, byrow=FALSE)
+      print(paste("d=", i, "=> valores propios:", round(eigen(S)$values[1],2), round(eigen(S)$values[2],2), round(eigen(S)$values[3],2), sep=" "))
+}
+
+
+d=0
+S=matrix(data=c(1+d,1,1,1,1+d,1,1,1,1+d), nrow=3, ncol=3, byrow=FALSE)
+eigen(S)
+
